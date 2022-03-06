43e44823d34348875f401f1438a8ec1985940b2f
kodluyoruz <baristutakli@hotmail.com>
2022-03-06 12:37:16 +0300

    CQRS With Mediator Pattern

---
+++ CQRSWithMediator/DesignPatterns.CQRSWithMediator/Interfaces/IMediator.cs
@@ -0,0 +1,19 @@
+﻿using DesignPatterns.CQRSWithMediator.RequestModels.CommandRequestModels;
+using DesignPatterns.CQRSWithMediator.RequestModels.QueryRequestModels;
+using DesignPatterns.CQRSWithMediator.ResponseModels.CommandResponseModels;
+using DesignPatterns.CQRSWithMediator.ResponseModels.QueryResponseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CQRSWithMediator.Interfaces
+{
+   public interface IMediator
+    {
+        public GetBookResponseModel Handle(GetBookRequestModel requestModel);
+        public CreateBookResponseModel Handle(CreateBookRequestModel requestModel);
+        
+    }
+}
